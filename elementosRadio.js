7f30d43fb92374cd2663047fb57b9a89a375e3fe
Claudio Raverta <claudioraverta@hotmail.com>
2020-04-25 18:40:51 -0300

    jugando un poco y probando cosas en JS

--- elementosRadio.js
+++ elementosRadio.js
@@ -1,9 +1,19 @@
 function elementosRadio(radio){
-window.addEventListener("click", (e) => {
-    console.log("You knocked?");
-    console.log(e.target);
-  });
-
-}
-
-elementosRadio(4);
+    window.addEventListener("click", (e) => {
+        let todos = document.getElementsByTagName("*");
+      
+    for (var i=0, max=todos.length; i < max; i++) {
+        //ESTE FOR RECORRE TODOS LOS ELEMENTOS DE LA PAGINA
+    }
+    
+    //ESTA PARTE TOMA LAS COORDENADAS DEL CLICK Y LAS IMPRIME EN PANTALLA
+    
+    var x = e.clientX;
+      var y = e.clientY;
+      var coords = "X coords: " + x + ", Y coords: " + y;
+    
+    
+    console.log(coords);  
+    }
+    )}
+    elementosRadio(4);
