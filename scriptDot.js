2f1dfd56215820be7fd2e39af854b9291c8632d1
Berceo Cano <canoyoberceo@gmail.com>
2020-05-13 02:12:53 -0300

    funcion de misclick con marcado de elementos en area

--- scriptDot.js
+++ scriptDot.js
@@ -1,8 +1,16 @@
 
+
     window.addEventListener("click", event => {
+
+        //crea div con position absuluta en los eje x e y
         let dot = document.createElement("div");
-        dot.className = "dot";
+        //dot.className = "dot";
+        dot.classList.add("dot");
+        dot.setAttribute("id","dot");
+
         dot.style.left = (event.pageX - 4) + "px";
         dot.style.top = (event.pageY - 4) + "px";
+        //y se lo asigna a document body
         document.body.appendChild(dot);
-      });   
+      
+      });
