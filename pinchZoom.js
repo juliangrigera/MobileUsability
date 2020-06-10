0e9a7ee01ea02918050b693d23c7970a9699ff9c
Claudio Raverta <claudioraverta@hotmail.com>
2020-06-10 13:00:33 -0300

    colorea elementos con pinch zoom

--- pinchZoom.js
+++ pinchZoom.js
@@ -35,19 +35,19 @@ function pointermove_handler(ev) {
  if (evCache.length == 2) {
    // Calculate the distance between the two pointers
    var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
-   document.getElementById("t1X").innerText = evCache[0].clientX.toFixed(2);
-   document.getElementById("t2X").innerText = evCache[1].clientX.toFixed(2);
-   document.getElementById("t1Y").innerText = evCache[0].clientY.toFixed(2);
-   document.getElementById("t2Y").innerText = evCache[1].clientY.toFixed(2);
+   document.getElementById("t1X").innerText = evCache[0].pageX.toFixed(2);
+   document.getElementById("t2X").innerText = evCache[1].pageX.toFixed(2);
+   document.getElementById("t1Y").innerText = evCache[0].pageY.toFixed(2);
+   document.getElementById("t2Y").innerText = evCache[1].pageY.toFixed(2);
    if (prevDiff > 0) {
      if (curDiff > prevDiff) {
        // The distance between the two pointers has increased
-  
+      elementsInPinch();
        zoomInfo.style.background  = "pink";
      }
      if (curDiff < prevDiff) {
        // The distance between the two pointers has decreased
-  
+       elementsInPinch();
        zoomInfo.style.background  = "lightblue";
      }
    }
@@ -62,10 +62,26 @@ function pointerup_handler(ev) {
   // Remove this pointer from the cache and reset the target's
   // background and border
   remove_event(ev);
-
+  
  
   // If the number of pointers down is less than two then reset diff tracker
-  if (evCache.length < 2) prevDiff = -1;
+  if (evCache.length < 2) {
+    prevDiff = -1;
+   
+  }
+}
+function pointerup2_handler(ev) {
+
+  // Remove this pointer from the cache and reset the target's
+  // background and border
+  remove_event(ev);
+  
+ 
+  // If the number of pointers down is less than two then reset diff tracker
+  if (evCache.length < 2) {
+    prevDiff = -1;
+
+  }
 }
 
 function remove_event(ev) {
@@ -78,8 +94,75 @@ function remove_event(ev) {
  }
 }
 
+function elementsInPinch(){
+ 
+  todos = document.getElementsByTagName("*");
+  let htmlElements = new Array();
+  console.log("Los elementos son: ");
+
+  for (var i=0, max=todos.length; i < max; i++) {
+      if( insidePinch(evCache[0].clientX, evCache[1].clientX, evCache[0].clientY, evCache[1].clientY, todos[i].getBoundingClientRect()) ){
+          console.log(todos[i]);
+
+    
+          todos[i].classList.add("boxShadowZoomOut");
+           htmlElements.push( todos[i] );
+      }
+  }
+  return htmlElements;
+}
+
+function insidePinch(x1, x2, y1, y2, elemRect){
+  if (x1 > x2){
+    aux = x1;
+    x1 = x2;
+    x2 = aux;
+  }
+
+  if (y1 > y2){
+    aux = y1;
+    y1 = y2;
+    y2 = aux;
+  }
+
+  if(elemRect.top >= y1 && elemRect.top <= y2){
+      return true;
+
+  }
+
+  
+  if(elemRect.bottom >= y1 && elemRect.bottom <= y2){
+    return true;
+
+}
+
+if(elemRect.left >= x1 && elemRect.left <= x2){
+  return true;
+
+}
 
 
+if(elemRect.right >= x1 && elemRect.right <= x2){
+return true;
+
+}
+
+return false
+
+
+}
+
+ function removeStyleElementsInRadioPrevious(){
+  //remover los elementos con boxShadow
+ 
+  let htmlItemsInRadio = document.getElementsByClassName("boxShadowZoomOut");
+  
+
+  for(i=0; i < htmlItemsInRadio.length ;i++){
+      htmlItemsInRadio[i].classList.remove("boxShadowZoomOut");
+  };
+
+}  
 
   var zoomInfo=document.getElementById("target");
   window.onpointerdown = pointerdown_handler;
@@ -92,4 +175,7 @@ function remove_event(ev) {
  window.onpointerout = pointerup_handler;
  window.onpointerleave = pointerup_handler;
 
-
+ document.addEventListener('touchend', (event) => {
+  removeStyleElementsInRadioPrevious();
+});
+ 
