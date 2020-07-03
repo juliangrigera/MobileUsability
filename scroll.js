b69d07babdbe62fdc2f0e5811b466392b52f775d
Berceo Cano <canoyoberceo@gmail.com>
2020-07-03 03:53:21 -0300

    envio de informe de evento scroll en formato json

--- scroll.js
+++ scroll.js
@@ -1,11 +1,16 @@
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+ 
+  var scrollTimerEventId;
+  var scrollPoints;
+
+  class ScrollPoints
+  {  
+    constructor(pointX, pointY)
+    {
+      this.pointX=pointX;
+      this.pointY=pointY;
+      //time  
     }
-    return color;
-  };
+  }
 
   document.getElementById("scrollTest").addEventListener("scroll",event => {
   
@@ -16,5 +21,43 @@ function getRandomColor() {
     document.getElementById ("scrollX").innerHTML =  x + "px";
     document.getElementById ("scrollY").innerHTML =  y + "px";
 
+    scrollPoints = new ScrollPoints(x,y);
+
+    setTimerScrollEvent();
+
   });   
 
+  function setTimerScrollEvent()
+  { //carga el nuevo evento y resetea el timer
+    clearTimeout(scrollTimerEventId);
+    scrollTimerEventId = window.setTimeout( sendDataScrollEvent2, 1500 );
+  }
+
+  function sendDataScrollEvent2()
+  {
+    console.log(">>>>>>>>>>> scroll Event <<<<<<<<<<<");
+    console.log("el objeto enviado es: ");
+    console.log( JSON.stringify(scrollPoints) );
+  }
+
+  function sendDataScrollEvent()
+  { 
+    const data = scrollPoints;
+
+    fetch('http://localhost:8000/ejemplo.php', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })                        //change recive json - text
+     .then(response => response.json())
+     .then(data => {
+       console.log('Success:', data);
+     })
+     .catch((error) => {
+       console.error('Error:', error);
+     });
+  }
+ 
+  
